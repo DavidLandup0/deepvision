817df4685c1f483812f465b01d1b4887175193b2
David Landup <david.landup.0@gmail.com>
2023-02-04 16:50:58 +0100

    tensorflow feature exploration

--- deepvision/evaluation/feature_analysis.py
+++ deepvision/evaluation/feature_analysis.py
@@ -14,88 +14,114 @@ class FeatureAnalyzer:
         self.backend = backend
         self.legend = legend
 
-    def visualize(self):
+    def process_dataset_tf(self):
         all_features = []
         all_classes = []
 
+        for index, batch in enumerate(self.dataset):
+            print(f"Processing batch {index}/{len(self.dataset)}", end="\r")
+            images, labels = batch
+
+            features = self.model(images)
+            all_features.append(features)
+            all_classes.append(labels)
+
+        print(f"\nProcessing finished. Extracting features and classes...")
+        all_classes_tf = tf.stack(all_classes)
+        all_classes_tf = tf.reshape(all_classes_tf.shape[0], -1)
+
+        all_features_tf = tf.stack(all_features)
+        all_features_tf = tf.reshape(all_features_tf.shape[0], -1)
+        classnames = tf.unique(all_classes_tf).numpy()
+
+        all_features = all_features_tf.numpy()
+        all_classes = all_classes_tf.numpy()
+        return all_features, all_classes, classnames
+
+    def process_dataset_pt(self):
+        all_features = []
+        all_classes = []
+
+        with torch.no_grad():
+            for index, batch in enumerate(self.dataset):
+                print(f"Processing batch {index}/{len(self.dataset)}", end="\r")
+                images, labels = batch
+                images = images.to(self.model.device)
+                labels = labels.to(self.model.device)
+
+                features = self.model(images)
+                all_features.append(features)
+                all_classes.append(labels)
+
+            print(f"\nProcessing finished. Extracting features and classes...")
+            all_classes_torch = torch.stack(all_classes)
+            all_classes_torch = all_classes_torch.flatten()
+            all_features_torch = torch.stack(all_features).reshape(
+                all_classes_torch.shape[0], -1
+            )
+            classnames = torch.unique(all_classes_torch).detach().cpu().numpy()
+
+            all_features = all_features_torch.detach().cpu().numpy()
+            all_classes = all_classes_torch.detach().cpu().numpy()
+
+            return all_features, all_classes, classnames
+
+    def visualize(self):
         if self.backend == "pytorch":
-            with torch.no_grad():
-                for index, batch in enumerate(self.dataset):
-                    print(f"Processing batch {index}/{len(self.dataset)}", end="\r")
-                    images, labels = batch
-                    images = images.to(self.model.device)
-                    labels = labels.to(self.model.device)
+            all_features, all_classes, classnames = self.process_dataset_pt()
+        else:
+            all_features, all_classes, classnames = self.process_dataset_tf()
+        print(f"Principal component analysis...")
+        pca = PCA(n_components=self.components)
+        features_pca = pca.fit_transform(all_features)
 
-                    features = self.model(images)
-                    all_features.append(features)
-                    all_classes.append(labels)
+        tsne = TSNE(
+            n_components=self.components,
+            verbose=1,
+            perplexity=75,
+            n_iter=1000,
+            metric="euclidean",
+        )
+        features_tsne = tsne.fit_transform(features_pca)
 
-                print(f"\nProcessing finished. Extracting features and classes...")
-                all_classes_torch = torch.stack(all_classes)
-                all_classes_torch = all_classes_torch.flatten()
-                all_features_torch = torch.stack(all_features).reshape(
-                    all_classes_torch.shape[0], -1
+        if self.components == 3:
+            fig = plt.figure(figsize=(10, 10))
+            ax = fig.add_subplot(121, projection="3d")
+            ax.set_title("Learned Feature PCA")
+            for class_id, classname in enumerate(classnames):
+                ax.scatter(
+                    features_pca[:, 0][all_classes == class_id],
+                    features_pca[:, 1][all_classes == class_id],
+                    features_pca[:, 2][all_classes == class_id],
+                    label=classname,
+                    alpha=0.4,
                 )
-                classnames = torch.unique(all_classes_torch).detach().cpu().numpy()
-
-                all_features = all_features_torch.detach().cpu().numpy()
-                all_classes = all_classes_torch.detach().cpu().numpy()
-
-                print(f"Principal component analysis...")
-                pca = PCA(n_components=self.components)
-                features_pca = pca.fit_transform(all_features)
-
-                tsne = TSNE(
-                    n_components=self.components,
-                    verbose=1,
-                    perplexity=75,
-                    n_iter=1000,
-                    metric="euclidean",
+            ax = fig.add_subplot(122, projection="3d")
+            ax.set_title("Learned Feature t-Stochastic Neighbor Embeddings")
+            ax.scatter(
+                features_tsne[:, 0],
+                features_tsne[:, 1],
+                features_tsne[:, 2],
+                c=all_classes,
+                cmap="coolwarm",
+            )
+        else:
+            fig, ax = plt.subplots(2, figsize=(10, 10))
+            ax[0].set_title("Learned Feature PCA")
+            ax[1].set_title("Learned Feature t-Stochastic Neighbor Embeddings")
+            for class_id, classname in enumerate(classnames):
+                ax[0].scatter(
+                    features_pca[:, 0][all_classes == class_id],
+                    features_pca[:, 1][all_classes == class_id],
+                    label=classname,
+                    alpha=0.4,
                 )
-                features_tsne = tsne.fit_transform(features_pca)
 
-                if self.components == 3:
-                    fig = plt.figure(figsize=(10, 10))
-                    ax = fig.add_subplot(121, projection="3d")
-                    ax.set_title("Learned Feature PCA")
-                    for class_id, classname in enumerate(classnames):
-                        ax.scatter(
-                            features_pca[:, 0][all_classes == class_id],
-                            features_pca[:, 1][all_classes == class_id],
-                            features_pca[:, 2][all_classes == class_id],
-                            label=classname,
-                            alpha=0.4,
-                        )
-                    ax = fig.add_subplot(122, projection="3d")
-                    ax.set_title("Learned Feature t-Stochastic Neighbor Embeddings")
-                    ax.scatter(
-                        features_tsne[:, 0],
-                        features_tsne[:, 1],
-                        features_tsne[:, 2],
-                        c=all_classes,
-                        cmap="coolwarm",
-                    )
-                else:
-                    fig, ax = plt.subplots(2, figsize=(10, 10))
-                    ax[0].set_title("Learned Feature PCA")
-                    ax[1].set_title("Learned Feature t-Stochastic Neighbor Embeddings")
-                    for class_id, classname in enumerate(classnames):
-                        ax[0].scatter(
-                            features_pca[:, 0][all_classes == class_id],
-                            features_pca[:, 1][all_classes == class_id],
-                            label=classname,
-                            alpha=0.4,
-                        )
+            ax[1].scatter(
+                features_tsne[:, 0],
+                features_tsne[:, 1],
+                c=all_classes,
+                cmap="coolwarm",
+            )
 
-                    ax[1].scatter(
-                        features_tsne[:, 0],
-                        features_tsne[:, 1],
-                        c=all_classes,
-                        cmap="coolwarm",
-                    )
-                """                
-                if self.legend:
-                    ax[0].legend()
-                    ax[1].legend()"""
-
-                plt.show()
+        plt.show()
