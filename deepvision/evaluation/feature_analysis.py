20d961cd841d4c41507c696ae5f2eab36d60ba6e
David Landup <david.landup.0@gmail.com>
2023-02-04 13:34:03 +0100

    sync

--- deepvision/evaluation/feature_analysis.py
+++ deepvision/evaluation/feature_analysis.py
@@ -2,5 +2,8 @@ import torch
 import tensorflow as tf
 
 class FeatureAnalyzer():
-    def __init__(self, model, backend):
+    def __init__(self, model, dataset, backend):
+        pass
+
+    def visualize(self):
         pass
