140dda115ea2fccb12c4f0e8203e47b44ce51b5e
David Landup <david.landup.0@gmail.com>
2023-02-04 14:20:07 +0100

    to np arrays

--- deepvision/evaluation/feature_analysis.py
+++ deepvision/evaluation/feature_analysis.py
@@ -35,19 +35,22 @@ class FeatureAnalyzer:
                 all_features_torch = torch.stack(all_features).reshape(
                     all_classes_torch.shape[0], -1
                 )
-                classnames = torch.unique(all_classes_torch)
+                classnames = torch.unique(all_classes_torch).detach().cpu().numpy()
+
+                all_features = all_features_torch.detach().cpu().numpy()
+                all_classes = all_classes_torch.detach().cpu().numpy()
 
                 print(f"Principal component analysis...")
                 pca = PCA(n_components=self.components)
-                features_pca = pca.fit_transform(all_features_torch.detach().cpu().numpy())
+                features_pca = pca.fit_transform(all_features)
 
                 plt.subplots(figsize=(10, 10))
 
                 for class_id, classname in enumerate(classnames):
                     plt.scatter(
-                        features_pca[:, 0][all_classes_torch == class_id],
-                        features_pca[:, 1][all_classes_torch == class_id],
-                        label=classname.detach().cpu().numpy(),
+                        features_pca[:, 0][all_classes == class_id],
+                        features_pca[:, 1][all_classes == class_id],
+                        label=classname,
                     )
 
                 plt.legend()
