771d0422c4fd810ef45f768e7f3ce8aae5f02730
David Landup <david.landup.0@gmail.com>
2023-02-04 14:11:51 +0100

    no grad

--- deepvision/evaluation/feature_analysis.py
+++ deepvision/evaluation/feature_analysis.py
@@ -11,44 +11,44 @@ class FeatureAnalyzer:
         self.components = components
         self.backend = backend
 
-        self.model.to("cpu")
+        #self.model.to("cpu")
 
     def visualize(self):
         all_features = []
         all_classes = []
 
         if self.backend == "pytorch":
+            with torch.no_grad():
+                for index, batch in enumerate(self.dataset):
+                    print(f"Processing batch {index}/{len(self.dataset)}", end="\r")
+                    images, labels = batch
+                    images = images.to(self.model.device)
+                    labels = labels.to(self.model.device)
 
-            for index, batch in enumerate(self.dataset):
-                print(f"Processing batch {index}/{len(self.dataset)}", end="\r")
-                images, labels = batch
-                images = images.to(self.model.device)
-                labels = labels.to(self.model.device)
+                    features = self.model(images)
+                    all_features.append(features)
+                    all_classes.append(labels)
 
-                features = self.model(images)
-                all_features.append(features)
-                all_classes.append(labels)
-
-            print(f"\nProcessing finished. Extracting features and classes...")
-            all_classes_torch = torch.stack(all_classes)
-            all_classes_torch = all_classes_torch.flatten()
-            all_features_torch = torch.stack(all_features).reshape(
-                all_classes_torch.shape[0], -1
-            )
-            classnames = torch.unique(all_classes_torch)
-
-            print(f"Principal component analysis...")
-            pca = PCA(n_components=self.components)
-            features_pca = pca.fit_transform(all_features_torch.detach().cpu().numpy())
-
-            plt.subplots(figsize=(10, 10))
-
-            for class_id, classname in enumerate(classnames):
-                plt.scatter(
-                    features_pca[:, 0][all_classes_torch == class_id],
-                    features_pca[:, 1][all_classes_torch == class_id],
-                    label=classname.detach().cpu().numpy(),
+                print(f"\nProcessing finished. Extracting features and classes...")
+                all_classes_torch = torch.stack(all_classes)
+                all_classes_torch = all_classes_torch.flatten()
+                all_features_torch = torch.stack(all_features).reshape(
+                    all_classes_torch.shape[0], -1
                 )
+                classnames = torch.unique(all_classes_torch)
 
-            plt.legend()
-            plt.show()
+                print(f"Principal component analysis...")
+                pca = PCA(n_components=self.components)
+                features_pca = pca.fit_transform(all_features_torch.detach().cpu().numpy())
+
+                plt.subplots(figsize=(10, 10))
+
+                for class_id, classname in enumerate(classnames):
+                    plt.scatter(
+                        features_pca[:, 0][all_classes_torch == class_id],
+                        features_pca[:, 1][all_classes_torch == class_id],
+                        label=classname.detach().cpu().numpy(),
+                    )
+
+                plt.legend()
+                plt.show()
