88df2f76b5c5b3f7a3df0981629244b767150022
David Landup <david.landup.0@gmail.com>
2023-02-07 11:51:51 +0100

    model configs formatting

--- deepvision/models/classification/efficientnet/efficientnetv2.py
+++ deepvision/models/classification/efficientnet/efficientnetv2.py
@@ -5,16 +5,18 @@ from deepvision.models.classification.efficientnet.efficientnetv2_tf import (
     EfficientNetV2TF,
 )
 
+# All configurations are block-wise, shortened to `block_N`
+# for cleaner formatting
 MODEL_CONFIGS = {
     "EfficientNetV2B0": {
-        "blockwise_kernel_sizes": [3, 3, 3, 3, 3, 3],
-        "blockwise_num_repeat": [1, 2, 2, 3, 5, 8],
-        "blockwise_input_filters": [32, 16, 32, 48, 96, 112],
-        "blockwise_output_filters": [16, 32, 48, 96, 112, 192],
-        "blockwise_expand_ratios": [1, 4, 4, 4, 6, 6],
-        "blockwise_se_ratios": [0, 0, 0, 0.25, 0.25, 0.25],
-        "blockwise_strides": [1, 2, 2, 2, 1, 2],
-        "blockwise_conv_type": [
+        "block_kernel_sizes": [3, 3, 3, 3, 3, 3],
+        "block_num_repeat": [1, 2, 2, 3, 5, 8],
+        "block_in_filters": [32, 16, 32, 48, 96, 112],
+        "block_out_filters": [16, 32, 48, 96, 112, 192],
+        "block_exp_ratios": [1, 4, 4, 4, 6, 6],
+        "block_se_ratios": [0, 0, 0, 0.25, 0.25, 0.25],
+        "block_strides": [1, 2, 2, 2, 1, 2],
+        "block_conv_type": [
             "fused",
             "fused",
             "fused",
@@ -50,21 +52,13 @@ def EfficientNetV2B0(
         pooling=pooling,
         width_coefficient=1.0,
         depth_coefficient=1.0,
-        blockwise_kernel_sizes=MODEL_CONFIGS["EfficientNetV2B0"][
-            "blockwise_kernel_sizes"
-        ],
-        blockwise_num_repeat=MODEL_CONFIGS["EfficientNetV2B0"]["blockwise_num_repeat"],
-        blockwise_input_filters=MODEL_CONFIGS["EfficientNetV2B0"][
-            "blockwise_input_filters"
-        ],
-        blockwise_output_filters=MODEL_CONFIGS["EfficientNetV2B0"][
-            "blockwise_output_filters"
-        ],
-        blockwise_expand_ratios=MODEL_CONFIGS["EfficientNetV2B0"][
-            "blockwise_expand_ratios"
-        ],
-        blockwise_se_ratios=MODEL_CONFIGS["EfficientNetV2B0"]["blockwise_se_ratios"],
-        blockwise_strides=MODEL_CONFIGS["EfficientNetV2B0"]["blockwise_strides"],
-        blockwise_conv_type=MODEL_CONFIGS["EfficientNetV2B0"]["blockwise_conv_type"],
+        blockwise_kernel_sizes=MODEL_CONFIGS["EfficientNetV2B0"]["block_kernel_sizes"],
+        blockwise_num_repeat=MODEL_CONFIGS["EfficientNetV2B0"]["block_num_repeat"],
+        blockwise_input_filters=MODEL_CONFIGS["EfficientNetV2B0"]["block_in_filters"],
+        blockwise_output_filters=MODEL_CONFIGS["EfficientNetV2B0"]["block_out_filters"],
+        blockwise_expand_ratios=MODEL_CONFIGS["EfficientNetV2B0"]["block_exp_ratios"],
+        blockwise_se_ratios=MODEL_CONFIGS["EfficientNetV2B0"]["block_se_ratios"],
+        blockwise_strides=MODEL_CONFIGS["EfficientNetV2B0"]["block_strides"],
+        blockwise_conv_type=MODEL_CONFIGS["EfficientNetV2B0"]["block_conv_type"],
     )
     return model
