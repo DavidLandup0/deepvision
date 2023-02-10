bb1098e6fc7c34c584b2f2cded2b220853558318
David Landup <60978046+DavidLandup0@users.noreply.github.com>
2023-02-10 23:08:08 +0100

    MBConv Weight Conversion (#70)
    * initial
    
    * checkpoint
    
    * mbconv tf to py
    
    * tf to pt mbconv finished
    
    * pt to tf mbconv
    
    * remove mapper template
    
    * add template back
    
    * remove temp

--- deepvision/layers/mbconv.py
+++ deepvision/layers/mbconv.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import tensorflow as tf
+import torch
 import torch.nn as nn
 from keras import backend
 from tensorflow.keras import layers
@@ -155,7 +156,7 @@ class __MBConvPT(nn.Module):
         strides=1,
         se_ratio=0.0,
         bn_momentum=0.9,
-        activation=nn.SiLU(),
+        activation=nn.SiLU,
         dropout: float = 0.8,
         name=None,  # Ignored but added for generalizability between backends
         **kwargs,
@@ -324,3 +325,222 @@ def MBConv(
     )
 
     return layer
+
+
+def tf_to_pt(layer, dummy_input):
+    """
+    Params:
+    layer: TensorFlow layer to convert weights from.
+    dummy_input: Dummy input, mimicking the expected input for the translated PyTorch layer.
+
+    Returns:
+        PyTorch MBConv block with weights transferred from the provided TensorFlow layer.
+    """
+    if not isinstance(layer, __MBConvTF):
+        raise ValueError(f"Layer type not supported, received: {type(layer)}")
+
+    # Pass dummy input through to
+    # get variables under `layer.variables`
+    layer(dummy_input)
+
+    pytorch_mbconv = __MBConvPT(
+        input_filters=layer.input_filters,
+        output_filters=layer.output_filters,
+        expand_ratio=layer.expand_ratio,
+        kernel_size=layer.kernel_size,
+        strides=layer.strides,
+        se_ratio=layer.se_ratio,
+        bn_momentum=layer.bn_momentum,
+        activation=torch.nn.SiLU,
+        dropout=layer.dropout,
+    )
+
+    if layer.expand_ratio != 1:
+        # conv1 and bn1
+        pytorch_mbconv.conv1.weight.data = torch.nn.Parameter(
+            torch.from_numpy(tf.transpose(layer.conv1.kernel, (3, 2, 0, 1)).numpy())
+        )
+        pytorch_mbconv.bn1.weight.data = torch.nn.Parameter(
+            torch.from_numpy(layer.bn1.gamma.numpy())
+        )
+        pytorch_mbconv.bn1.bias.data = torch.nn.Parameter(
+            torch.from_numpy(layer.bn1.beta.numpy())
+        )
+        pytorch_mbconv.bn1.running_mean.data = torch.nn.Parameter(
+            torch.from_numpy(layer.bn1.moving_mean.numpy())
+        )
+        pytorch_mbconv.bn1.running_var.data = torch.nn.Parameter(
+            torch.from_numpy(layer.bn1.moving_variance.numpy())
+        )
+
+    # Depthwise
+    pytorch_mbconv.depthwise.weight.data = torch.nn.Parameter(
+        torch.from_numpy(
+            tf.transpose(layer.depthwise.depthwise_kernel, (2, 3, 0, 1)).numpy()
+        )
+    )
+
+    pytorch_mbconv.bn2.weight.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn2.gamma.numpy())
+    )
+    pytorch_mbconv.bn2.bias.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn2.beta.numpy())
+    )
+    pytorch_mbconv.bn2.running_mean.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn2.moving_mean.numpy())
+    )
+    pytorch_mbconv.bn2.running_var.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn2.moving_variance.numpy())
+    )
+
+    if 0 < layer.se_ratio <= 1:
+        pytorch_mbconv.se_conv1.weight.data = torch.nn.Parameter(
+            torch.from_numpy(tf.transpose(layer.se_conv1.kernel, (3, 2, 0, 1)).numpy())
+        )
+        pytorch_mbconv.se_conv1.bias.data = torch.nn.Parameter(
+            torch.from_numpy(layer.se_conv1.bias.numpy())
+        )
+        pytorch_mbconv.se_conv2.weight.data = torch.nn.Parameter(
+            torch.from_numpy(tf.transpose(layer.se_conv2.kernel, (3, 2, 0, 1)).numpy())
+        )
+        pytorch_mbconv.se_conv2.bias.data = torch.nn.Parameter(
+            torch.from_numpy(layer.se_conv2.bias.numpy())
+        )
+    pytorch_mbconv.output_conv.weight.data = torch.nn.Parameter(
+        torch.from_numpy(tf.transpose(layer.output_conv.kernel, (3, 2, 0, 1)).numpy())
+    )
+
+    pytorch_mbconv.bn3.weight.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn3.gamma.numpy())
+    )
+    pytorch_mbconv.bn3.bias.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn3.beta.numpy())
+    )
+    pytorch_mbconv.bn3.running_mean.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn3.moving_mean.numpy())
+    )
+    pytorch_mbconv.bn3.running_var.data = torch.nn.Parameter(
+        torch.from_numpy(layer.bn3.moving_variance.numpy())
+    )
+
+    return pytorch_mbconv
+
+
+def pt_to_tf(layer, dummy_input):
+    """
+    Params:
+    layer: PyTorch layer to convert weights from.
+    dummy_input: Dummy input, mimicking the expected input for the translated TensorFlow layer.
+
+    Returns:
+        TensorFlow MBConv block with weights transferred from the provided PyTorch layer.
+    """
+    if not isinstance(layer, __MBConvPT):
+        raise ValueError(f"Layer type not supported, received: {type(layer)}")
+
+    tensorflow_mbconv = __MBConvTF(
+        input_filters=layer.input_filters,
+        output_filters=layer.output_filters,
+        expand_ratio=layer.expand_ratio,
+        kernel_size=layer.kernel_size,
+        strides=layer.stride,
+        se_ratio=layer.se_ratio,
+        bn_momentum=layer.bn_momentum,
+        activation=tf.keras.activations.swish,
+        dropout=layer.dropout,
+    )
+
+    # Pass dummy input through to
+    # get variables under `layer.variables`
+    tf_dummy_input = tf.convert_to_tensor(
+        dummy_input.detach().cpu().numpy().transpose(0, 2, 3, 1)
+    )
+    tensorflow_mbconv(tf_dummy_input)
+
+    if layer.expand_ratio != 1:
+        # conv1 and bn1
+        tensorflow_mbconv.conv1.kernel.assign(
+            tf.convert_to_tensor(
+                layer.conv1.weight.data.permute(2, 3, 1, 0).detach().cpu().numpy()
+            )
+        )
+        tensorflow_mbconv.bn1.gamma.assign(
+            tf.convert_to_tensor(layer.bn1.weight.data.detach().cpu().numpy())
+        )
+
+        tensorflow_mbconv.bn1.beta.assign(
+            tf.convert_to_tensor(layer.bn1.bias.data.detach().cpu().numpy())
+        )
+
+        tensorflow_mbconv.bn1.moving_mean.assign(
+            tf.convert_to_tensor(layer.bn1.running_mean.data.detach().cpu().numpy())
+        )
+
+        tensorflow_mbconv.bn1.moving_variance.assign(
+            tf.convert_to_tensor(layer.bn1.running_var.data.detach().cpu().numpy())
+        )
+
+    # Depthwise
+    tensorflow_mbconv.depthwise.depthwise_kernel.assign(
+        tf.convert_to_tensor(
+            layer.depthwise.weight.data.permute(2, 3, 0, 1).detach().cpu().numpy()
+        )
+    )
+
+    tensorflow_mbconv.bn2.gamma.assign(
+        tf.convert_to_tensor(layer.bn2.weight.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn2.beta.assign(
+        tf.convert_to_tensor(layer.bn2.bias.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn2.moving_mean.assign(
+        tf.convert_to_tensor(layer.bn2.running_mean.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn2.moving_variance.assign(
+        tf.convert_to_tensor(layer.bn2.running_var.data.detach().cpu().numpy())
+    )
+
+    if 0 < layer.se_ratio <= 1:
+        tensorflow_mbconv.se_conv1.kernel.assign(
+            tf.convert_to_tensor(
+                layer.se_conv1.weight.data.permute(2, 3, 1, 0).detach().cpu().numpy()
+            )
+        )
+        tensorflow_mbconv.se_conv1.bias.assign(
+            tf.convert_to_tensor(layer.se_conv1.bias.data.detach().cpu().numpy())
+        )
+        tensorflow_mbconv.se_conv2.kernel.assign(
+            tf.convert_to_tensor(
+                layer.se_conv2.weight.data.permute(2, 3, 1, 0).detach().cpu().numpy()
+            )
+        )
+        tensorflow_mbconv.se_conv2.bias.assign(
+            tf.convert_to_tensor(layer.se_conv2.bias.data.detach().cpu().numpy())
+        )
+
+    tensorflow_mbconv.output_conv.kernel.assign(
+        tf.convert_to_tensor(
+            layer.output_conv.weight.data.permute(2, 3, 1, 0).detach().cpu().numpy()
+        )
+    )
+
+    tensorflow_mbconv.bn3.gamma.assign(
+        tf.convert_to_tensor(layer.bn3.weight.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn3.beta.assign(
+        tf.convert_to_tensor(layer.bn3.bias.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn3.moving_mean.assign(
+        tf.convert_to_tensor(layer.bn3.running_mean.data.detach().cpu().numpy())
+    )
+
+    tensorflow_mbconv.bn3.moving_variance.assign(
+        tf.convert_to_tensor(layer.bn3.running_var.data.detach().cpu().numpy())
+    )
+
+    return tensorflow_mbconv
