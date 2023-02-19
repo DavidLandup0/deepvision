9f2d0805c1ec0f6443ef46a72923a5bf01c14794
DavidLandup0 <david.landup.0@gmail.com>
2023-02-19 17:25:17 +0100

    add nerf template

--- deepvision/models/volumetric_rendering/nerf/nerf.py
+++ deepvision/models/volumetric_rendering/nerf/nerf.py
@@ -26,11 +26,8 @@ MODEL_BACKBONES = {"tensorflow": NeRFTF, "pytorch": NeRFPT}
 
 def NeRF(
     backend,
-    include_top,
-    classes=None,
     input_shape=(None, None, 3),
     input_tensor=None,
-    pooling=None,
     **kwargs,
 ):
     model_class = MODEL_BACKBONES.get(backend)
@@ -39,18 +36,8 @@ def NeRF(
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
     model = model_class(
-        include_top,
         input_shape=input_shape,
         input_tensor=input_tensor,
-        pooling=pooling,
-        classes=classes,
-        patch_size=MODEL_CONFIGS["ViTTiny16"]["patch_size"],
-        transformer_layer_num=MODEL_CONFIGS["ViTTiny16"]["transformer_layer_num"],
-        project_dim=MODEL_CONFIGS["ViTTiny16"]["project_dim"],
-        mlp_dim=MODEL_CONFIGS["ViTTiny16"]["mlp_dim"],
-        num_heads=MODEL_CONFIGS["ViTTiny16"]["num_heads"],
-        mlp_dropout=MODEL_CONFIGS["ViTTiny16"]["mlp_dropout"],
-        attention_dropout=MODEL_CONFIGS["ViTTiny16"]["attention_dropout"],
         **kwargs,
     )
 
