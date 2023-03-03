01902adab85d50e8d22ced1e3803dd919a2a9ffe
David Landup <david.landup.0@gmail.com>
2023-03-03 01:25:59 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name="deepvision-toolkit",
-    version="0.1.3",
-    description="PyTorch and TensorFlow/Keras image models with automatic weight conversions and equal API/implementations - Vision Transformer (ViT), ResNetV2, EfficientNetV2, (planned...) DeepLabV3+, ConvNeXtV2, YOLO, NeRF, etc.",
+    version="0.1.5",
+    description="PyTorch and TensorFlow/Keras image models with automatic weight conversions and equal API/implementations - Vision Transformer (ViT), ResNetV2, EfficientNetV2, NeRFs, (planned...) DeepLabV3+, ConvNeXtV2, YOLO, etc.",
     url="https://github.com/DavidLandup0/deepvision",
     author="David Landup",
     author_email="david.landup.0@gmail.com",
