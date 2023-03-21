744a87d17c9fb210febf7714db239d299445a0d1
DavidLandup0 <david.landup.0@gmail.com>
2023-03-21 17:47:27 +0100

    tensorflow segformer finished

--- deepvision/models/segmentation/segformer/segformer.py
+++ deepvision/models/segmentation/segformer/segformer.py
@@ -1,7 +1,8 @@
 from deepvision.models.segmentation.backbones.mit import mit
 from deepvision.models.segmentation.segformer.segformer_pt import __SegFormerPT
+from deepvision.models.segmentation.segformer.segformer_tf import __SegFormerTF
 
-MODEL_BACKBONES = {"tensorflow": None, "pytorch": __SegFormerPT}
+MODEL_BACKBONES = {"tensorflow": __SegFormerTF, "pytorch": __SegFormerPT}
 
 
 def SegFormerB0(input_shape, num_classes, backend, softmax_output=True):
@@ -11,9 +12,9 @@ def SegFormerB0(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB0(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB0(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=256,
+        embed_dim=256,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
@@ -27,9 +28,9 @@ def SegFormerB1(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB1(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB1(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=256,
+        embed_dim=256,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
@@ -43,9 +44,9 @@ def SegFormerB2(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB2(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB2(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=768,
+        embed_dim=768,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
@@ -59,9 +60,9 @@ def SegFormerB3(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB3(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB3(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=768,
+        embed_dim=768,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
@@ -75,9 +76,9 @@ def SegFormerB4(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB4(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB4(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=768,
+        embed_dim=768,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
@@ -91,9 +92,9 @@ def SegFormerB5(input_shape, num_classes, backend, softmax_output=True):
             f"Backend not supported: {backend}. Supported backbones are {MODEL_BACKBONES.keys()}"
         )
 
-    backbone = mit.MiTB5(input_shape=input_shape, backend="pytorch")
+    backbone = mit.MiTB5(input_shape=input_shape, backend=backend)
     return model_class(
-        embed_dims=768,
+        embed_dim=768,
         num_classes=num_classes,
         softmax_output=softmax_output,
         backbone=backbone,
--- deepvision/models/segmentation/segformer/segformer_tf.py
+++ deepvision/models/segmentation/segformer/segformer_tf.py
@@ -1,11 +1,9 @@
-import torch
-from torch import Tensor
-from torch.nn import functional as F
+import tensorflow as tf
 
 from deepvision.layers.segformer_segmentation_head import SegFormerHead
 
 
-class __SegFormerPT(torch.nn.Module):
+class __SegFormerTF(tf.keras.Model):
     def __init__(self, num_classes, backbone, embed_dim, softmax_output=None):
         super().__init__()
         self.backbone = backbone
@@ -13,16 +11,14 @@ class __SegFormerPT(torch.nn.Module):
             in_dims=backbone.output_channels,
             embed_dim=embed_dim,
             num_classes=num_classes,
-            backend="pytorch",
+            backend="tensorflow",
         )
         self.softmax_output = softmax_output
 
-    def forward(self, x):
+    def call(self, x):
         y = self.backbone(x)
         y = self.decode_head(y)
-        y = F.interpolate(
-            y, size=x.shape[2:], mode="bilinear", align_corners=False
-        )  # to original image shape
+        y = tf.image.resize(y, size=[x.shape[1], x.shape[2]], method="bilinear")
         if self.softmax_output:
-            y = torch.nn.Softmax(1)(y)
+            y = tf.nn.softmax(y, axis=1)
         return y
